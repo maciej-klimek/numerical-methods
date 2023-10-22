562933576e5d4eff94a6355458a7ee7eee0ffcf9
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2023-10-22 23:05:17 +0200

    lab03.1

--- L03_matrix/problem3_7.m
+++ L03_matrix/problem3_7.m
@@ -11,24 +11,24 @@ figure; plot(x); title('x(n)');
 n=0:N-1; k=0:N-1;
 A = sqrt(2/N)*cos( pi/N *(k'*n));
 %x = A(500,:) + A(1000,:); x = x';
-y = A*x;
+y = A*x; %widmo sygnału
 figure; plot(y); title('y(k)');
 
 % Modyfikacja wyniku
-s = 3000;
-e = length(x) - 1000;
-y(s:e,1) = zeros(e-s+1,1);
-% y(1000)=0;
+
+% od = 3000;
+% do = length(x) - 1000;
+% y(od:do,1) = zeros(do-od+1,1);
+
 % y = A(500,:) + A(1000,:);
-y(1000) = 0;
+
+% y(1000) = 0;
+
 figure; plot(y); title('y(k)');
 
 % Transformacja odwrotna - SYNTEZA
-xback = A'*y';
+xback = A'*y;
 figure; plot(xback); title('xback(n)');
 
-
-length(y)
-length(A)
 soundsc(x,fpr); pause
 soundsc(xback,fpr);
