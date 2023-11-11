30f021f27f520f21211741410e41544b149d5ca8
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2023-11-11 11:14:16 +0100

    minor changes

--- L04_equlin/problem4_5.m
+++ L04_equlin/problem4_5.m
@@ -1,17 +1,14 @@
 clear all; close all; clc; format long;
 
 A = [6, 1, 2; 5, 7, 1; 8, 9, 0];
-
 D = NaN(size(A));
-
 for c=1:size(A,1)
   for r=1:size(A,2)
-    D(c,r) = det(A([1:c-1 c+1:end],[1:r-1 r+1:end]));
+    D(c,r) = (-1)^(c+r) * det(A([1:c-1 c+1:end],[1:r-1 r+1:end]));
   end
 end
 
 D = D';
-
 A_inverse = 1/(det(A)) * D
 inv(A)
 ,
