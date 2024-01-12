bf0e8e28e0fbacfba1e6ccab5331a050e27ac14a
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-01-12 20:22:20 +0100

    minor

--- L15_fixedpoint/2 zadanie/fractional2binary.m
+++
@@ -1,9 +0,0 @@
-format long
-
-a = 1234.57849; % your float point number
-n = 16;         % number bits for integer part of your number      
-m = 20;         % number bits for fraction part of your number
-% binary number
-d2b = [ fix(rem(fix(a)*pow2(-(n-1):0),2)), fix(rem( rem(a,1)*pow2(1:m),2))],  % 
-% the inverse transformation
-b2d = d2b*pow2([n-1:-1:0 -(1:m)].'),
